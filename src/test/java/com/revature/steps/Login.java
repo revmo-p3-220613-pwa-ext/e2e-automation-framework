3765b98c4713f3a2c214c7eab2c2d4725387366b
Jace Lenon <jace.lenon@yahoo.com>
2022-08-19 15:57:03 -0400

    Update pom.xml

--- src/test/java/com/revature/steps/Login.java
+++ src/test/java/com/revature/steps/Login.java
@@ -13,42 +13,36 @@ import org.testng.Assert;
 import java.time.Duration;
 
 import static com.revature.runner.TestRunner.driver;
-import static java.lang.Thread.sleep;
 
 public class Login {
 
     public LoginPage loginPage;
 
     @Given("I am at the  login page")
-    public void iAmAtTheLoginPage() throws InterruptedException {
+    public void iAmAtTheLoginPage() {
         driver.get("http://127.0.0.1:5501/login.html");
         loginPage = new LoginPage(driver);
-        sleep(4000);
     }
 
     @When("I type in a email of {string}")
-    public void iTypeInAEmailOf(String email) throws InterruptedException {
+    public void iTypeInAEmailOf(String email){
         loginPage.typeEmail(email);
-        sleep(4000);
     }
 
     @And("I type in a password of {string}")
-    public void iTypeInAPasswordOf(String password) throws InterruptedException {
+    public void iTypeInAPasswordOf(String password) {
         loginPage.typePassword(password);
-        sleep(4000);
     }
 
     @And("I click the login button")
-    public void iClickTheLoginButton() throws InterruptedException {
+    public void iClickTheLoginButton() {
         loginPage.clickLoginButton2();
-        sleep(4000);
     }
 
     @Then("I should be redirected to the student homepage")
-    public void iShouldBeRedirectedToTheStudentHomepage() throws InterruptedException {
+    public void iShouldBeRedirectedToTheStudentHomepage() {
         WebDriverWait wdw = new WebDriverWait(driver, Duration.ofSeconds(10));
         wdw.until(ExpectedConditions.urlToBe("http://127.0.0.1:5501/user-page.html"));
         Assert.assertEquals(driver.getCurrentUrl(), "http://127.0.0.1:5501/user-page.html");
-        sleep(2000);
     }
 }
