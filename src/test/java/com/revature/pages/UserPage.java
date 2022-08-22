b9fecaef0e3b6c672a190c19b6c8332c6538f23f
scott <scottsvance7@gmail.com>
2022-08-22 12:30:08 -0400

    add automated tests for going to the user edit info page and for editng a user's info (broke login test - sorry Jace)

--- src/test/java/com/revature/pages/UserPage.java
+++ src/test/java/com/revature/pages/UserPage.java
@@ -3,7 +3,11 @@ package com.revature.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class UserPage {
 
@@ -21,5 +25,26 @@ public class UserPage {
 
     @FindBy(id="phone")
     private WebElement phoneNumber;
+
+    @FindBy(id="edit-user-info-btn")
+    private WebElement editUserInfoButton;
+
+    @FindBy(id="hello")
+    private WebElement hello;
+
+    public UserPage(WebDriver driver) {
+        this.driver = driver;
+        this.wdw = new WebDriverWait(driver, Duration.ofSeconds(2));
+        PageFactory.initElements(driver, this);
+    }
+
+    public void getHello() {
+        wdw.until(ExpectedConditions.urlContains("user-page"));
+        hello.getText();
+    }
+
+    public void clickEditUserInfoButton() {
+        editUserInfoButton.click();
+    }
 
 }
--- src/test/java/com/revature/steps/Login.java
+++ src/test/java/com/revature/steps/Login.java
@@ -9,6 +9,7 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -19,27 +20,32 @@ public class Login {
     public LoginPage loginPage;
 
     @Given("I am at the  login page")
+    @Test(groups = "login")
     public void iAmAtTheLoginPage() {
         driver.get("http://127.0.0.1:5501/login.html");
         loginPage = new LoginPage(driver);
     }
 
     @When("I type in a email of {string}")
+    @Test(groups = "login")
     public void iTypeInAEmailOf(String email){
         loginPage.typeEmail(email);
     }
 
     @And("I type in a password of {string}")
+    @Test(groups = "login")
     public void iTypeInAPasswordOf(String password) {
         loginPage.typePassword(password);
     }
 
     @And("I click the login button")
+    @Test(groups = "login")
     public void iClickTheLoginButton() {
         loginPage.clickLoginButton2();
     }
 
     @Then("I should be redirected to the student homepage")
+    @Test(groups = "login")
     public void iShouldBeRedirectedToTheStudentHomepage() {
         WebDriverWait wdw = new WebDriverWait(driver, Duration.ofSeconds(10));
         wdw.until(ExpectedConditions.urlToBe("http://127.0.0.1:5501/user-page.html"));
